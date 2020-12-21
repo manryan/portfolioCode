a0114d78a38ab3c1a48e9df2559d2b545466ec39
manryan <44647816+manryan@users.noreply.github.com>
2020-12-20 17:26:23 -0800

    Add files via upload

---
+++ beat em up/attackctrlPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackctrlPlayer : AttackController
+{
+    Player myp;
+    public override void knockedOver(GameObject obj)
+    {
+        playa.floored = true;
+        base.knockedOver(obj);
+
+        //tell enemies in my player manager to leave me alone/keep their distance/etc for a bit?
+        myp.leaveMeAlone();
+        
+    }
+
+    private void Start()
+    {
+        myp = (Player)playa;
+    }
+
+}
